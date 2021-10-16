b946f60ebe7c26d464d44c14b01f48e0e43c5021
Nahuel <gamarraorlandonahuel@gmail.com>
2021-10-16 00:26:07 -0300

    ahora si esta agregado la sube

---
+++ src/ar/edu/unlam/pb2/lineaFerroviria/Sube.java
@@ -0,0 +1,5 @@
+package ar.edu.unlam.pb2.lineaFerroviria;
+
+public class Sube {
+	private Double saldo;
+}
---
+++ test/ar/edu/unlam/pb2/lineaFerroviria/SubeTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.lineaFerroviria;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SubeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
